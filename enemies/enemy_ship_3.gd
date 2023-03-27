7fcddc8577f7df71c707b09f6d25dde2ad7b84cd
Antonio Dell'Annunziata <contact@antonio-da.dev>
2023-03-27 21:08:20 +0200

    feat: Add states to bomber; Create pulsating shader

--- enemies/enemy_ship_3.gd
+++ enemies/enemy_ship_3.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends CharacterBody3D
 
 
 enum State {MOVE_INTO_SCREEN, FIND_TARGET, CHASE_TARGET, SHOW_DAMAGE_AREA, EXPLODE}
@@ -7,12 +7,59 @@ enum State {MOVE_INTO_SCREEN, FIND_TARGET, CHASE_TARGET, SHOW_DAMAGE_AREA, EXPLO
 var _state = State.MOVE_INTO_SCREEN
 
 
-func _process(_delta: float):
+@onready var _follow_curve_component := $follow_curve_component as FollowCurveComponent
+@onready var _visible_on_screen_notifier_3d := $VisibleOnScreenNotifier3D as VisibleOnScreenNotifier3D
+@onready var _nearest_target_finder_component := $nearest_target_finder_component as NearestTargetFinder
+@onready var _velocity_component := $velocity_component as VelocityComponent
+@onready var _player_detect_area := $PlayerDetectArea as Area3D
+
+
+func _physics_process(_delta: float):
 	match _state:
-		State.MOVE_INTO_SCREEN: pass
-		State.FIND_TARGET: pass
-		State.CHASE_TARGET: pass
+		State.MOVE_INTO_SCREEN: _move_into_screen()
+		State.FIND_TARGET: _find_target()
+		State.CHASE_TARGET: _chase_target()
 		State.SHOW_DAMAGE_AREA: pass
 		State.EXPLODE: pass
 
 
+func _move_into_screen():
+	if _follow_curve_component.is_progressing: return 
+	_follow_curve_component.start_follow()
+	await _visible_on_screen_notifier_3d.screen_entered
+	await get_tree().create_timer(5.0).timeout
+	_follow_curve_component.stop_follow()
+	_state = State.FIND_TARGET
+
+
+func _find_target():
+	_nearest_target_finder_component.find_nearest_target(global_position)
+	_state = State.CHASE_TARGET
+
+
+func _on_target_lost():
+	if _state >= State.SHOW_DAMAGE_AREA: return
+	_state = State.FIND_TARGET
+
+
+func _chase_target():
+	var direction = _nearest_target_finder_component.target.global_position - global_position
+	_velocity_component.direction = direction
+	move_and_slide()
+
+
+func _on_velocity_changed(new_velocity, acceleration):
+	velocity = new_velocity
+
+
+func _on_player_detect_area_entered(area):
+	if _state > State.CHASE_TARGET: return
+	_state = State.SHOW_DAMAGE_AREA
+
+
+func _show_damage_area():
+	pass
+
+
+func _explode():
+	pass
--- enemies/follow_curve_component.gd
+++ enemies/follow_curve_component.gd
@@ -3,30 +3,30 @@ extends Path3D
 
 
 @export var progress_units_per_second := 1.0
+var is_progressing := false
 
 
-var _is_progressing := false
 @onready var _path_follow := $PathFollow3D
 
 
 func start_follow():
-	if _is_progressing or not curve: return
+	if is_progressing or not curve: return
 	var parent_to_reparent = get_parent_node_3d()
 	reparent(parent_to_reparent.get_parent_node_3d())
 	parent_to_reparent.reparent($PathFollow3D)
-	_is_progressing = true
+	is_progressing = true
 
 
 func stop_follow():
-	if not _is_progressing: return
-	_is_progressing = false
+	if not is_progressing: return
+	is_progressing = false
 	var original_parent = $PathFollow3D.get_child(0)
 	original_parent.reparent(get_parent_node_3d())
 	reparent(original_parent)
 
 
 func _physics_process(delta):
-	if not _is_progressing: return
+	if not is_progressing: return
 	
 	_path_follow.progress += progress_units_per_second * delta
 	
